63a6b426f421fc9af876d7d23f00b59cb386f042
Devon Hinton <dhinton@stanford.edu>
2015-07-07 14:11:17 -0400

    add context to load tracks call, fix argumetns typo in ajax jack, hook up, working besides getting 268/273

--- scrape8tracks.js
+++ scrape8tracks.js
@@ -10,24 +10,24 @@ function Download8tracks(className){
   this.grabbing=0;
   this.grabbed=0;
   this.hijackRequest();
-  this.saveAllUrls();
+  this.loadAllTracks(this.saveAllUrls,this);
 }
-Download8tracks.prototype.8tracksSongPerPage='20';
-Download8tracks.prototype.requestAllTracks = function(cb){
+Download8tracks.prototype.defaultSongsPerPage=20;
+Download8tracks.prototype.loadAllTracks = function(cb,context){
   var moreBtn=$('.more')[0],
       numTracks=parseInt($('.favs_count').text()),
       cbCalled=false, //For Backup
       listener=function(){
         numTracks--;
-        if(numTracks===20){
-          // cbCalled=true;
+        if(numTracks===this.defaultSongsPerPage){ //Does not reload already present songs
+          cbCalled=true;
           $(window).off('DOMNodeInserted',listener);
-          // cb();
+          cb.apply(context||this,[]);
         }
       },
       href=moreBtn.href;
 
-  moreBtn.href=href.replace('20',numTracks).replace('2','1');
+  moreBtn.href=href.replace(this.defaultSongsPerPage,numTracks).replace('2','1');
 
   $(window).on('DOMNodeInserted',listener);
   moreBtn.click();
@@ -36,7 +36,7 @@ Download8tracks.prototype.requestAllTracks = function(cb){
     if(!cbCalled){
       cbCalled=true;
       $(window).off('DOMNodeInserted',listener);
-      cb();
+      cb.apply(context||this,[]);
     }
   }, 4000);
 }
@@ -64,7 +64,7 @@ Download8tracks.prototype.hijackRequest = function(){
       argHsh.context=self;
       _ajax.apply($,[argHsh]);
     } else {
-      _ajax.apply($,arguements);
+      _ajax.apply($,arguments);
     }
   }
 }
