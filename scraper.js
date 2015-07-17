d24f1e652efc36e22b53a88359d4bb6807825a8c
Devon Hinton <dhinton@stanford.edu>
2015-07-17 12:07:38 -0400

    so much cleaner, #sa-weet

--- scraper.js
+++ scraper.js
@@ -1,5 +1,19 @@
-Scraper.prototype.setupListeners = function(){
-  var self=this;
+// Case where there are no more tracks :)
+
+function Scraper(className){
+  this.ids=[];
+  this.finished=0;
+  this.className=className||'youtube_link';
+  this.videConversationSite='http://www.vidtomp3.com/';
+
+  this.setupListeners();
+  this.loadAllTracks(this.saveAllUrls,this);
+}
+
+Scraper.prototype.setupListeners = function(){ // Scraper scope $ is different than 8track's $ (JQuery)
+  var self=this;                               // This code allows hijacking of the correct JQuery
+                                               // Chrome Extensions cannot have access to window scope
+                                               // Hence the event passing and script injection
   document.addEventListener('idReturned',function(evt){
     self.ids.push(evt.detail);
     self.finished++;
@@ -14,55 +28,53 @@ Scraper.prototype.setupListeners = function(){
   Helper.injectScript(chrome.extension.getURL('getJQuery.js'), 'body');
 }
 
-Scraper.prototype.saveUrls = function(){
-  alert('saving urls')
-  var data={};
-
-  data[STORAGE_ID]=this.ids.join(' ');
-  chrome.storage.sync.set(data);
-}
-
-function Scraper(className){
-  this.ids=[];
-  this.finished=0;
-  this.className=className||'youtube_link';
-  this.videConversationSite='http://www.vidtomp3.com/';
-
-  this.setupListeners();
-  this.loadAllTracks(this.saveAllUrls,this);
-}
-Scraper.prototype.defaultSongsPerPage=20;
-Scraper.prototype.youtubeURLBase="http://www.youtube.com/v/";
 Scraper.prototype.loadAllTracks = function(cb,context){
   var moreBtn=$('.more')[0],
       numTracks=parseInt($('.favs_count').text()),
       cbCalled=false, //For Backup
-      listener=function(){
-        numTracks--;
-        if(numTracks===this.defaultSongsPerPage){ //Does not reload already present songs
-          debugger;
+      moveOn=function(){
           cbCalled=true;
           $(window).off('DOMNodeInserted',listener);
           cb.apply(context||this,[]);
-        }
+      },
+      listener=function(){
+        numTracks--;
+        if(numTracks===this.defaultSongsPerPage) //Does not reload already present songs
+          moveOn();
+      },
+      backup=function(){
+        if(!cbCalled)
+          moveOn();
       },
       href=moreBtn.href;
 
-  moreBtn.href=href.replace(this.defaultSongsPerPage,numTracks).replace('2','1');
+  moreBtn.href=href.replace(this.defaultSongsPerPage,numTracks)
+                   .replace('2','1'); // Make button load all tracks
 
   $(window).on('DOMNodeInserted',listener);
   moreBtn.click();
 
-  setTimeout(function(){ //Backup
-    if(!cbCalled){
-      cbCalled=true;
-      $(window).off('DOMNodeInserted',listener);
-      cb.apply(context||this,[]);
-    }
-  }, 4000);
+  setTimeout(backup,8000);
 }
+
+Scraper.prototype.saveUrls = function(){
+  var data={},
+      self=this;
+
+  data[STORAGE_ID]=this.ids.join(' ');
+  chrome.storage.sync.set(data,function(){
+    alert('saved ' + self.ids.length + ' ids, now going to download');
+    self.goToDownload();
+  });
+}
+
+Scraper.prototype.goToDownload = function(){
+  window.location=this.videConversationSite;
+}
+Scraper.prototype.defaultSongsPerPage=20;
+Scraper.prototype.youtubeURLBase="http://www.youtube.com/v/";
+
 Scraper.prototype.saveAllUrls = function(cb,context){
-  alert('about to download urls')
   this.elems=document.getElementsByClassName(this.className);
   this.total=this.total=this.elems.length;
 
