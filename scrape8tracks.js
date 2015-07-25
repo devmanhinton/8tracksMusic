33426714f76c66d4ae0a5858c574c3a2a6d6609a
Devon Hinton <dhinton@stanford.edu>
2015-07-25 17:02:38 -0400

    extension now responds to on/off setting when scraping or download

--- scrape8tracks.js
+++ scrape8tracks.js
@@ -1,7 +1,16 @@
 var scraper,
     downloader;
 
-if(window.location.origin.match('8tracks'))
-  scraper=new Scraper();
-else
-  downloader=new Downloader();
+
+chrome.storage.sync.get(SETTINGS,function(data){
+    if(!data || !data[SETTINGS] || data[SETTINGS].status==='off')
+        return
+    else {
+      console.log('extension is turned on!');
+      if(window.location.origin.match('8tracks'))
+        scraper=new Scraper();
+      else
+        downloader=new Downloader();
+    }
+});
+
