080fa59a55cc010360c0755792e16a6c1c1e9845
Devon Hinton <dhinton@stanford.edu>
2015-07-27 12:49:11 -0400

    remove unessecary console.log

--- scrape8tracks.js
+++ scrape8tracks.js
@@ -5,7 +5,6 @@ chrome.storage.sync.get(SETTINGS,function(data){
     if(!data || !data[SETTINGS] || data[SETTINGS].status==='off')
         return
     else {
-      console.log('extension is turned on!');
       if(window.location.origin.match('8tracks'))
         scraper=new Scraper();
       else
